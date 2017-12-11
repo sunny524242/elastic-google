905474f2de66867af5243f44b8f8faf62a65c8f7
chenrenhao <1045617063@qq.com>
2017-12-11 13:40:36 +0800

    测试修改git姓名

--- src/main/java/com/example/es/EsApplication.java
+++ src/main/java/com/example/es/EsApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * ES搜索应用范例
+ * ES搜索-应用范例
  */
 @SpringBootApplication
 public class EsApplication {
